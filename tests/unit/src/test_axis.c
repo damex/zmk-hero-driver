3650b1834d55ff817f9ef8cac1a51bc28c83650a
Roman Kuzmitskii <roman@damex.org>
2026-05-30 16:52:41 +0700

    #2: unit test(s) for hero_axis_flags

--- tests/unit/src/test_axis.c
+++ tests/unit/src/test_axis.c
@@ -18,6 +18,16 @@ static void assert_transform(uint32_t flags, int16_t in_x, int16_t in_y, int16_t
     zassert_equal(delta_y, expect_y, "y: flags=%u got=%d want=%d", flags, delta_y, expect_y);
 }
 
+ZTEST(hero_axis, test_axis_flags) {
+    zassert_equal(hero_axis_flags(false, false, false), 0, "no flags");
+    zassert_equal(hero_axis_flags(true, false, false), HERO_AXIS_FLAG_INVERT_X, NULL);
+    zassert_equal(hero_axis_flags(false, true, false), HERO_AXIS_FLAG_INVERT_Y, NULL);
+    zassert_equal(hero_axis_flags(false, false, true), HERO_AXIS_FLAG_SWAP_XY, NULL);
+    zassert_equal(hero_axis_flags(true, true, true),
+                  HERO_AXIS_FLAG_INVERT_X | HERO_AXIS_FLAG_INVERT_Y | HERO_AXIS_FLAG_SWAP_XY,
+                  "all flags");
+}
+
 ZTEST(hero_axis, test_axis_transform) {
     assert_transform(0, 3, 7, 3, 7);
     assert_transform(HERO_AXIS_FLAG_INVERT_X, 3, 7, -3, 7);
